2548b1e7b42584db3c8e12e6c1cbe1107e7d55f7
Yuna Tomida <yuna.tomida@akariinc.co.jp>
2024-12-31 17:50:25 +0900

    add shell

---
+++ raycast_command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Expand URL for Perplexity/Felo etc.
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📄
+
+# Documentation:
+# @raycast.author Yuna Tomida
+
+./markdown_citation_expander
